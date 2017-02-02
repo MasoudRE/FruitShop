d0bc56768277daaf79a06d4b5c2911c0347b4cc6
Abn Najaf <abn.najaf@gmail.com>
2017-02-02 20:57:55 +0330

    Edit show product

--- FruitShop/Default.aspx.cs
+++ FruitShop/Default.aspx.cs
@@ -10,28 +10,6 @@ public partial class _Default : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-        using (FruitShopEntity db = new FruitShopEntity())
-        {
-            db.Mahsolats.Add(new Mahsolat()
-            {
-                Description = "",
-            });
-
-
-            db.Mivehs.Add(new DataLayer.Models.Miveh()
-            {
-                Amount = "4"
-            });
-
-
-            db.Sabzijats.Add(new Sabzijat()
-            {
-                Amount = "2"
-            });
-
-            db.SaveChanges();
-        }
-
-        Response.Redirect("ShowProduct.aspx");
+        Response.Redirect("/SitePanel/ShowProduct.aspx");
     }
 }
--- FruitShop/Theme/Slider/Slider.js
+++ FruitShop/Theme/Slider/Slider.js
@@ -1,31 +1,5 @@
 ﻿$(document).ready(function () {
-    $('#myCarousel').carousel({
-        interval: 10000,
+    $('#Carousel').carousel({
+        interval: 5000
     })
-
-
-    $('.fdi-Carousel .item').each(function () {
-        var next = $(this).next();
-        if (!next.length) {
-            next = $(this).siblings(':first');
-        }
-        next.children(':first-child').clone().appendTo($(this));
-
-        if (next.next().length > 0) {
-            next.next().children(':first-child').clone().appendTo($(this));
-
-            if (next.next().next().length > 0) {
-                next.next().next().children(':first-child').clone().appendTo($(this));
-            }
-            else {
-                $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-            }
-        }
-        else {
-            $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-        }
-    });
-   
 });
-
-
