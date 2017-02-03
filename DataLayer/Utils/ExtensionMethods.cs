ec2c00a029d8b0abce2c044b727200ba74d97604
Abn Najaf <abn.najaf@gmail.com>
2017-02-03 17:14:34 +0330

    edit

---
+++ DataLayer/Utils/ExtensionMethods.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Utils
+{
+    public static class ExtensionMethods
+    {
+        public static bool IsNullOrEmpty(this string str)
+        {
+            try
+            {
+                return string.IsNullOrEmpty(str);
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public static bool IsNullOrFalse(this string str)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(str))
+                    return true;
+
+                if (bool.Parse(str) == false)
+                    return true;
+
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ FruitShop/Theme/Price/price.js
@@ -0,0 +1,83 @@
+﻿$(document).ready(function () {
+
+    $('span.price').each(function () {
+
+        $(this).text(removeComma($(this).text()));
+
+    });
+    $('span.price').each(function () {
+  
+        $(this).text(addComma($(this).text()));
+       
+    });
+
+  
+
+    $(".price").blur(function () {
+
+        if (this.value.length <= 0) {
+            $(this).removeClass("counting-input dirty");
+            $(this).addClass("counting-input");
+        }
+ 
+     
+                    return this.value = addComma(this.value);
+
+                });
+      
+    $(".price").focus(function () {
+   
+        $(this).removeClass("counting-input");
+        $(this).addClass("counting-input dirty");
+                    return this.value = removeComma(this.value);
+                });
+            });
+       
+        
+
+            function addComma(str) {
+       
+                var objregex = new RegExp('(-?[0-9]+)([0-9]{3})');
+
+                while (objregex.test(str)) {
+                    str = str.replace(objregex, '$1,$2');
+                }
+   
+                if (str.length>0) {
+                    return str + ' ریال ';
+                }
+                else
+                {
+                    return str ;
+                }
+             
+
+
+            }
+
+            function removeComma(str) {
+
+                str = str.replace(/,/g, '');
+                str = str.replace(' ریال ', '');
+                return str;
+            }
+
+            $(".price").keydown(function (e) {
+                // Allow: backspace, delete, tab, escape, enter and .
+                if ($.inArray(e.keyCode, [46, 8, 9, 27, 13, 110, 190]) !== -1 ||
+                    // Allow: Ctrl+A
+                    (e.keyCode == 65 && e.ctrlKey === true) ||
+                    // Allow: Ctrl+C
+                    (e.keyCode == 67 && e.ctrlKey === true) ||
+                    // Allow: Ctrl+X
+                    (e.keyCode == 88 && e.ctrlKey === true) ||
+                    // Allow: home, end, left, right
+                    (e.keyCode >= 35 && e.keyCode <= 39)) {
+                    // let it happen, don't do anything
+                    return;
+                }
+                // Ensure that it is a number and stop the keypress
+                if ((e.shiftKey || (e.keyCode < 48 || e.keyCode > 57)) && (e.keyCode < 96 || e.keyCode > 105)) {
+                    e.preventDefault();
+                }
+            });
